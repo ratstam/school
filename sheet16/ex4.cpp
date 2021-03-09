53f8c82b56bf1dbadd657948075b15dd46109b72
MatStar15 <39085851+MatStar15@users.noreply.github.com>
2021-03-09 18:48:49 +0100

    Create ex4.cpp

---
+++ sheet16/ex4.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+
+double min (double x, double y){
+	if(x < y)
+		return x;
+	else
+		return y;
+}
+
+double max (double x, double y){
+		if(x > y)
+		return x;
+	else
+		return y;
+}
+
+
+int main(){
+	double min_num, max_num, num[4];
+
+	for(int i = 0; i < num.size; i++){
+		cout<< "\n Number: ";
+		cin >> num[i];
+	}
+
+	min_num = min(num[1], num[2])
+	for (int i = 3; i < num.size; i++)
+	{
+		min_num = min(min_num, num[i])
+	}
+
+	max_num = max(num[1], num[2])
+	for (int i = 3; i < num.size; i++)
+	{
+		max_num = max(max_num, num[i])
+	}
+	
+	cout << "Min: " << min_num << "\nMax: " << max_num;
+}
