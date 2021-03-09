0dd68c2892a1d27b1ba16200a19f54962de8d77b
MatStar15 <39085851+MatStar15@users.noreply.github.com>
2021-03-09 18:33:29 +0100

    Create ex2.cpp

---
+++ sheet16/ex2.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	int n, repetitions[9] = {0, 0, 0, 0, 0, 0, 0, 0, 0};
+
+	do {
+		cout << "\nNumber: " ;
+		cin >> n;
+
+
+		repetitions[n]++
+	}while(n>=0 && n<=8);
+
+	cout << "\n Repetitions:" <<endl;
+	for(int i = 0; i <=8; i++){
+		cout << " Number " << i << " was repeated " << repetitions[i]<< " times\n"
+	}
+}
