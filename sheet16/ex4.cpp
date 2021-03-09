750d9f78daa11200f5a36a13fa88432a69a6f08f
MatStar15 <39085851+MatStar15@users.noreply.github.com>
2021-03-09 18:58:56 +0100

    Update ex4.cpp

--- sheet16/ex4.cpp
+++ sheet16/ex4.cpp
@@ -18,22 +18,24 @@ double max (double x, double y){
 
 
 int main(){
-	double min_num, max_num, num[4];
+	double min_num, max_num;
+	double num[4];
 
-	for(int i = 0; i < num.size; i++){
+	for(int i = 0; i < 4; i++){
 		cout<< "\n Number: ";
 		cin >> num[i];
 	}
 
-	min_num = min(num[1], num[2]);
-	for (int i = 3; i < size(num); i++){
+	min_num = min(num[0], num[1]);
+	for (int i = 2; i < 4; i++){
 		min_num = min(min_num, num[i]);
 	}
 
-	max_num = max(num[1], num[2]);
-	for (int i = 3; i < size(num); i++){
+	max_num = max(num[0], num[1]);
+	for (int i = 2; i < 4; i++){
 		max_num = max(max_num, num[i]);
 	}
 	
-	cout << "Min: " << min_num << "\nMax: " << max_num;
+	cout << "Min: " << min_num << "\nMax: " << max_num<< endl;
+	cout <<min(num[0], num[1]);
 }
