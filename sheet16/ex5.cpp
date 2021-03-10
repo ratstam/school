2df60003db02157c7ea30c87a0406600fcf380ef
MatStar15 <39085851+MatStar15@users.noreply.github.com>
2021-03-10 10:17:49 +0100

    Update ex5.cpp

---
+++ sheet16/ex5.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+using namespace std;
+
+bool is_perfect (int k){
+	int cont = 1, sum = 0;	
+
+
+	while(cont <= k/2){
+		if(k%cont == 0)
+			sum += cont;
+		cont++;
+		}
+
+	return (sum == k);
+}
+
+
+bool branchless_is_perfect(int k){
+	int cont = 1, sum = 0;
+	while(cont <= k/2){
+		sum += cont*(k%cont == 0);
+		cont ++;
+	}
+
+	return (sum == k);
+}
+
+
+int main(){
+	while (true){
+		int n_max = 1;
+
+		cout << "\nRange: ";
+		cin >> n_max;
+
+		if(n_max >= 6){
+			int k = 6;
+
+			cout << "\nPerfect numbers in range 1-" << n_max << ":\n";
+
+			while(k <= n_max){
+				if(branchless_is_perfect(k) == true){
+					cout << k <<" ";
+				}
+				k++;
+			}
+		}
+		else{
+			cout << "\nThere aren't any perfect numbers in this range" << endl;
+			}
+
+	}
+}
