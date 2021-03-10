5b222293eb51221d4c20c06d0979531fe97deb39
MatStar15 <39085851+MatStar15@users.noreply.github.com>
2021-03-10 16:46:51 +0100

    Update functions1.cpp

--- sheet16/functions1.cpp
+++ sheet16/functions1.cpp
@@ -11,17 +11,18 @@ int mcd (int n,int m){ // This functions return the gcd(int) of n(int) and m(int
 int main(){
 	int n,d,dc;
 	char operazione;
-	cout << "\n\n DIGITA N / D: ";
+
+	cout << "\n DIGITA N / D: ";
 	cin >> n >> operazione >> d;
 	dc = mcd(n,d);
-	cout << "\n" << dc << "\n";
 	cout << "\n                   " << n << " " << operazione << " " << d << " = ";
 	if (d==0) cout << "OPERAZIONE IMPOSSIBILE"<< endl;
 	else switch (operazione){
 				case ':':
 				case '/':
-					if( dc==1) cout << " GIA' SEMPLIFICATA";
-					else cout << n/dc << "/" << d/dc << endl; break;
+					if( dc==1){ cout << " GIA' SEMPLIFICATA"; break;}
+					else if(d/dc == 1){ cout << n/dc; break;}
+					else{ cout << n/dc << "/" << d/dc << endl; break;}
 				default: cout << "OPERAZIONE SCONOSCIUTA"<< endl; break;
 			}
 }
