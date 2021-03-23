3269de0bd9e903ae0271fb8942e1a2d35831dade
MatStar15 <39085851+MatStar15@users.noreply.github.com>
2021-03-23 19:59:19 +0100

    Create ex3.cpp

---
+++ sheet18/ex3.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+ int reverse_integer (int n){
+	 int rev_n = 0;
+
+	 while (n > 0) {
+        rev_n = rev_n * 10 + n % 10;
+        n = n / 10;
+    }
+    return rev_n;
+ }
+
+int main(){
+	int n, reversed_n;
+	
+	cout << "Number to reverse:";
+	cin >> n;
+
+	reversed_n = reverse_integer(n);
+
+	cout << "\nResult: " << reversed_n;
+}
